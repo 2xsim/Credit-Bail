85a6d017eebb931e0818f56bfe484a85e88c8b73
mikeprince <mikeprince@Mikeprince-HP-EliteBook-Folio-1040-G1>
2021-08-22 22:27:06 +0000

    First commit

---
+++ src/main/java/com/mmp/creditbail/config/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package com.mmp.creditbail.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+public class ApplicationConfiguration {
+}
